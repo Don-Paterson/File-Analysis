71f24df457466585c23adf226fef386abb3cfd9a
Don-Paterson <donpaterson@hotmail.com>
2026-03-31 15:09:20 +0100

    Update run-file-analysis.ps1

--- run-file-analysis.ps1
+++ run-file-analysis.ps1
@@ -1,4 +1,4 @@
-#$dest = "$env:USERPROFILE\Desktop\Analyse-Files.ps1"
+#$dest = "$env:USERPROFILE\Desktop\Analyse-Files.ps1" 
 $dest = "$([Environment]::GetFolderPath('Desktop'))\Analyse-Files.ps1"
 Invoke-RestMethod "https://raw.githubusercontent.com/Don-Paterson/File-Analysis/main/Analyse-Files.ps1" |
     Out-File -FilePath $dest -Encoding UTF8 -NoNewline
